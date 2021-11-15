d022e9dd8afadf863cfd23597c04b464f27e69ad
Hafidh Pradipta A <hamstergeek38@gmail.com>
2021-11-15 14:35:37 +0700

    remove unnecessray stuff

--- src/Infrastructures/repository/CommentRepositoryPostgres.js
+++ src/Infrastructures/repository/CommentRepositoryPostgres.js
@@ -66,48 +66,6 @@ class CommentRepositoryPostgres extends CommentRepository {
     const result = await this._pool.query(query);
     return result.rows.map(comment => new DetailComment(mapDBToModel(comment)));
   }
-
-  async likeCommentById({ commentId, userId }) {
-    const id = `like-${this._idGenerator(16)}`;
-
-    const query = {
-      text: `
-        INSERT INTO likes VALUES($1, $2, $3) RETURNING id
-      `,
-      values: [id, commentId, userId],
-    };
-
-    const result = await this._pool.query(query);
-    return result.rows[0];
-  }
-
-  async unlikeCommentById({ commentId, userId }) {
-    const query = {
-      text: `
-        DELETE FROM likes WHERE id_comments = $1 AND id_users = $2 RETURNING id
-      `,
-      values: [commentId, userId],
-    };
-
-    const result = await this._pool.query(query);
-    return result.rows[0];
-  }
-
-  async checkLikeStatus({ commentId, userId }) {
-    const query = {
-      text: `
-        SELECT id FROM likes WHERE id_comments = $1 AND id_users = $2
-      `,
-      values: [commentId, userId],
-    };
-
-    const result = await this._pool.query(query);
-    if (result.rowCount) {
-      const { id } = result.rows[0];
-      return !!id;
-    }
-    return false;
-  }
 }
 
 module.exports = CommentRepositoryPostgres;
